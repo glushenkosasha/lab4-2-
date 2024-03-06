76f7bea3348855c9398eca6d63b35695d6194213
Hlushenko Oleksandra <glushenkosasha@knu.ua>
2024-03-06 16:15:47 +0200

    Додано функцію is_power_of_5 у файл utils.py

--- utils.py
+++ utils.py
@@ -13,3 +13,13 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
+
+
+def is_power_of_5(n):
+
+
+    if n == 0:
+        return False
+    while n % 5 == 0:
+        n /= 5
+    return n == 1
