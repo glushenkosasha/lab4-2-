4c1b057c945133ff4b910a1c977c9220e24bfbdf
Hlushenko Oleksandra <glushenkosasha@knu.ua>
2024-03-06 16:19:32 +0200

    Перенесення змін з гілки dev5 на гілку main

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from utils import is_prime
+from utils import is_prime, is_power_of_5, is_power_of_2
 
 number = 17
 if is_prime(number):
     print(f"{number} є простим числом")
 else:
     print(f"{number} не є простим числом")
+
+number = 125
+if is_power_of_5(number):
+    print(f"{number} є степенем 5")
+else:
+    print(f"{number} не є степенем 5")
+
+number = 16
+if is_power_of_2(number):
+    print(f"{number} є степенем 2")
+else:
+    print(f"{number} не є степенем 2")
+
--- utils.py
+++ utils.py
@@ -12,4 +12,21 @@ def is_prime(n):
     for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
             return False
+<<<<<<< Updated upstream
     return True
+=======
+    return True
+
+
+def is_power_of_5(n):
+
+
+    if n == 0:
+        return False
+    while n % 5 == 0:
+        n /= 5
+    return n == 1
+
+def is_power_of_2(n):
+    return n > 0 and (n & (n - 1)) == 0
+>>>>>>> Stashed changes
